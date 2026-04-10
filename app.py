1efcfe201c13c836526a09b8a4ef8c293d2f7fc8
yenbisikir-design <yenbisikir@gmail.com>
2026-04-10 03:11:28 +0300

    Update app.run to use environment variable for port

--- app.py
+++ app.py
@@ -56,6 +56,7 @@ def bildirim():
 @app.route('/')
 def index():
     return "Ziraat Bot çalışıyor ✅"
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
